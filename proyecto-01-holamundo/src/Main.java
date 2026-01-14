f6979b9e8fa9aba26d94a0333e004080340be5d1
dulce canseco <20253ds119@utez.edu.mx>
2026-01-13 21:14:14 -0600

    proyecto 01: hola mundo

---
+++ proyecto-01-holamundo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Nombre: Dulce Canseco");
+        System.out.println("Grupo: 2D");
+        System.out.println("Fecha: 13/01/2026");
+        System.out.println("Hola mundo");
+    }
+}
